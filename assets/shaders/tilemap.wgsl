45caa440a880600900fd8158f3b50a39ee22745f
zhulin <alexniver@aliyun.com>
2024-08-31 14:38:08 +0800

    draw tile

--- assets/shaders/tilemap.wgsl
+++ assets/shaders/tilemap.wgsl
@@ -6,12 +6,12 @@
 struct VertexIn {
     @builtin(instance_index) instance_index: u32,
     @location(0) position: vec3<f32>,
-    @location(1) relative_position: vec2<f32>,
+    @location(1) uv: vec2<f32>,
 }
 
 struct VertexOut {
     @builtin(position) clip_position: vec4<f32>,
-    @location(0) relative_position: vec2<f32>,
+    @location(0) uv: vec2<f32>,
 }
 
 struct TileInfoUniform {
@@ -19,24 +19,23 @@ struct TileInfoUniform {
     row: u32,
 };
 
-@group(2) @binding(0) var<uniform> tile_size: TileInfoUniform;
-@group(2) @binding(1) var base_color_texture: texture_2d<f32>;
-@group(2) @binding(2) var base_color_sampler: sampler;
+@group(2) @binding(0) var<uniform> tile_info: TileInfoUniform;
+@group(2) @binding(1) var texture: texture_2d<f32>;
+@group(2) @binding(2) var texture_sampler: sampler;
 
 
 @vertex
 fn vertex(in: VertexIn) -> VertexOut {
     var out: VertexOut;
-    out.clip_position = mesh_functions::mesh2d_position_world_to_clip(vec4<f32>(in.position, 1.0));
-    out.relative_position = in.relative_position;
+    // out.clip_position = mesh_functions::mesh2d_position_world_to_clip(vec4<f32>(in.position, 1.0));
+    out.clip_position = vec4f(in.position, 1.0);
+    out.uv = in.uv;
     return out;
 }
 
 
 @fragment
 fn fragment(in: VertexOut) -> @location(0) vec4<f32> {
-    return vec4<f32>(in.relative_position, 0.0, 1.0);
-    // return in.clip_position;
-    // return vec4f(0.5, 0.5, 0.0, 1.0);
-    // return vec4f(1.0, 1.0, 0.0, 1.0);
+    var uv = in.uv / vec2f(8.0, 2.0);
+    return textureSample(texture, texture_sampler, uv);
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ use bevy::{
         mesh::{Indices, MeshVertexAttribute, PrimitiveTopology},
         render_asset::RenderAssetUsages,
         render_resource::{AsBindGroup, ShaderRef, ShaderType, VertexFormat},
+        texture::{
+            ImageAddressMode, ImageFilterMode, ImageLoaderSettings, ImageSampler,
+            ImageSamplerDescriptor,
+        },
     },
     sprite::{Material2d, Material2dPlugin, MaterialMesh2dBundle},
 };
@@ -20,8 +24,12 @@ fn main() {
         .add_systems(Startup, setup)
         .run();
 }
-pub const ATTRIBUTE_RELATIVE_POSITION: MeshVertexAttribute =
-    MeshVertexAttribute::new("Relative_Position", 1, VertexFormat::Float32x2);
+
+pub const ATTRIBUTE_POS: MeshVertexAttribute =
+    MeshVertexAttribute::new("Position", 0, VertexFormat::Float32x3);
+
+pub const ATTRIBUTE_UV: MeshVertexAttribute =
+    MeshVertexAttribute::new("UV", 1, VertexFormat::Float32x2);
 
 // Setup a simple 2d scene
 fn setup(
@@ -38,32 +46,66 @@ fn setup(
         RenderAssetUsages::RENDER_WORLD,
     );
 
-    let width = 16. * 16.;
-    let height = 16. * 16.;
+    // let width = 16. * 16.;
+    // let height = 16. * 16.;
+
+    // let width = 16. * 4.;
+    // let height = 16. * 4.;
+    // let width = 16.;
+    // let height = 16.;
+    // let width = 1.;
+    // let height = 1.;
 
     // These vertices are specified in 3D space.
+    // let v_pos = vec![
+    //     [0.0 * width, 0.0 * height, 0.0],
+    //     [1.0 * width, 0.0 * height, 0.0],
+    //     [1.0 * width, 1.0 * height, 0.0],
+    //     [0.0 * width, 1.0 * height, 0.0],
+    // ];
+
+    // let v_pos = vec![
+    //     [-0.5 * width, -0.5 * height, 0.0],
+    //     [0.5 * width, -0.5 * height, 0.0],
+    //     [0.5 * width, 0.5 * height, 0.0],
+    //     [-0.5 * width, 0.5 * height, 0.0],
+    // ];
     let v_pos = vec![
-        [0.0 * width, 0.0 * height, 0.0],
-        [1.0 * width, 0.0 * height, 0.0],
-        [1.0 * width, 1.0 * height, 0.0],
-        [0.0 * width, 1.0 * height, 0.0],
+        [-0.5, -0.5, 0.0],
+        [0.5, -0.5, 0.0],
+        [0.5, 0.5, 0.0],
+        [-0.5, 0.5, 0.0],
     ];
 
-    let v_relative_pos = vec![[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0]];
+    let uvs = vec![[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0]];
 
     // Set the position attribute
-    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, v_pos);
-    mesh.insert_attribute(ATTRIBUTE_RELATIVE_POSITION, v_relative_pos);
+    mesh.insert_attribute(ATTRIBUTE_POS, v_pos);
+    mesh.insert_attribute(ATTRIBUTE_UV, uvs);
 
     let indices = vec![0, 1, 3, 1, 2, 3];
     mesh.insert_indices(Indices::U32(indices));
+    let image_handle = asset_server.load_with_settings(
+        "images/tilemap.png",
+        |settings: &mut ImageLoaderSettings| {
+            settings.sampler = ImageSampler::Descriptor(ImageSamplerDescriptor {
+                mag_filter: ImageFilterMode::Nearest,
+                min_filter: ImageFilterMode::Nearest,
+                mipmap_filter: ImageFilterMode::Nearest,
+                address_mode_u: ImageAddressMode::ClampToEdge,
+                address_mode_v: ImageAddressMode::ClampToEdge,
+                address_mode_w: ImageAddressMode::ClampToEdge,
+                ..Default::default()
+            });
+        },
+    );
 
     // quad
     commands.spawn(MaterialMesh2dBundle {
         mesh: meshes.add(mesh).into(),
         material: materials.add(TileMapMaterial {
-            tile_size: TileSizeUniform { col: 5, row: 1 },
-            color_texture: Some(asset_server.load("images/tilemap.png")),
+            tile_size: TileSizeUniform { col: 8, row: 2 },
+            texture: Some(image_handle),
         }),
         ..default()
     });
@@ -76,7 +118,7 @@ struct TileMapMaterial {
     tile_size: TileSizeUniform,
     #[texture(1)]
     #[sampler(2)]
-    color_texture: Option<Handle<Image>>,
+    texture: Option<Handle<Image>>,
 }
 
 #[derive(Debug, ShaderType, Clone)]
@@ -86,8 +128,6 @@ struct TileSizeUniform {
     row: u32,
 }
 
-/// The Material2d trait is very configurable, but comes with sensible defaults for all methods.
-/// You only need to implement functions for features that need non-default behavior. See the Material2d api docs for details!
 impl Material2d for TileMapMaterial {
     fn vertex_shader() -> ShaderRef {
         "shaders/tilemap.wgsl".into()
