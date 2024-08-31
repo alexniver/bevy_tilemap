a7fbc6238ab558eb03cf01d47529ffb45d3d1baa
zhulin <alexniver@aliyun.com>
2024-08-31 08:17:44 +0800

    relative position

--- assets/shaders/tilemap.wgsl
+++ assets/shaders/tilemap.wgsl
@@ -1,12 +1,42 @@
-#import bevy_sprite::mesh2d_vertex_output::VertexOutput
-// we can import items from shader modules in the assets folder with a quoted path
-#import "shaders/custom_material_import.wgsl"::COLOR_MULTIPLIER
+#import bevy_sprite::{
+    mesh2d_functions as mesh_functions,
+    mesh2d_view_bindings::view,
+}
 
-@group(2) @binding(0) var<uniform> material_color: vec4<f32>;
+struct VertexIn {
+    @builtin(instance_index) instance_index: u32,
+    @location(0) position: vec3<f32>,
+    @location(1) relative_position: vec2<f32>,
+}
+
+struct VertexOut {
+    @builtin(position) clip_position: vec4<f32>,
+    @location(0) relative_position: vec2<f32>,
+}
+
+struct TileInfoUniform {
+    col: u32,
+    row: u32,
+};
+
+@group(2) @binding(0) var<uniform> tile_size: TileInfoUniform;
 @group(2) @binding(1) var base_color_texture: texture_2d<f32>;
 @group(2) @binding(2) var base_color_sampler: sampler;
 
+
+@vertex
+fn vertex(in: VertexIn) -> VertexOut {
+    var out: VertexOut;
+    out.clip_position = mesh_functions::mesh2d_position_world_to_clip(vec4<f32>(in.position, 1.0));
+    out.relative_position = in.relative_position;
+    return out;
+}
+
+
 @fragment
-fn fragment(mesh: VertexOutput) -> @location(0) vec4<f32> {
-    return material_color * textureSample(base_color_texture, base_color_sampler, mesh.uv) * COLOR_MULTIPLIER;
+fn fragment(in: VertexOut) -> @location(0) vec4<f32> {
+    return vec4<f32>(in.relative_position, 0.0, 1.0);
+    // return in.clip_position;
+    // return vec4f(0.5, 0.5, 0.0, 1.0);
+    // return vec4f(1.0, 1.0, 0.0, 1.0);
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@
 use bevy::{
     prelude::*,
     reflect::TypePath,
-    render::render_resource::{AsBindGroup, ShaderRef},
+    render::{
+        mesh::{Indices, MeshVertexAttribute, PrimitiveTopology},
+        render_asset::RenderAssetUsages,
+        render_resource::{AsBindGroup, ShaderRef, ShaderType, VertexFormat},
+    },
     sprite::{Material2d, Material2dPlugin, MaterialMesh2dBundle},
 };
 
@@ -16,6 +20,8 @@ fn main() {
         .add_systems(Startup, setup)
         .run();
 }
+pub const ATTRIBUTE_RELATIVE_POSITION: MeshVertexAttribute =
+    MeshVertexAttribute::new("Relative_Position", 1, VertexFormat::Float32x2);
 
 // Setup a simple 2d scene
 fn setup(
@@ -27,12 +33,37 @@ fn setup(
     // camera
     commands.spawn(Camera2dBundle::default());
 
+    let mut mesh = Mesh::new(
+        PrimitiveTopology::TriangleList,
+        RenderAssetUsages::RENDER_WORLD,
+    );
+
+    let width = 16. * 16.;
+    let height = 16. * 16.;
+
+    // These vertices are specified in 3D space.
+    let v_pos = vec![
+        [0.0 * width, 0.0 * height, 0.0],
+        [1.0 * width, 0.0 * height, 0.0],
+        [1.0 * width, 1.0 * height, 0.0],
+        [0.0 * width, 1.0 * height, 0.0],
+    ];
+
+    let v_relative_pos = vec![[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0]];
+
+    // Set the position attribute
+    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, v_pos);
+    mesh.insert_attribute(ATTRIBUTE_RELATIVE_POSITION, v_relative_pos);
+
+    let indices = vec![0, 1, 3, 1, 2, 3];
+    mesh.insert_indices(Indices::U32(indices));
+
     // quad
     commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(Rectangle::default()).into(),
+        mesh: meshes.add(mesh).into(),
         material: materials.add(TileMapMaterial {
-            color: LinearRgba::BLUE,
-            color_texture: Some(asset_server.load("branding/icon.png")),
+            tile_size: TileSizeUniform { col: 5, row: 1 },
+            color_texture: Some(asset_server.load("images/tilemap.png")),
         }),
         ..default()
     });
@@ -42,15 +73,25 @@ fn setup(
 #[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
 struct TileMapMaterial {
     #[uniform(0)]
-    color: LinearRgba,
+    tile_size: TileSizeUniform,
     #[texture(1)]
     #[sampler(2)]
     color_texture: Option<Handle<Image>>,
 }
 
+#[derive(Debug, ShaderType, Clone)]
+#[repr(C)]
+struct TileSizeUniform {
+    col: u32,
+    row: u32,
+}
+
 /// The Material2d trait is very configurable, but comes with sensible defaults for all methods.
 /// You only need to implement functions for features that need non-default behavior. See the Material2d api docs for details!
 impl Material2d for TileMapMaterial {
+    fn vertex_shader() -> ShaderRef {
+        "shaders/tilemap.wgsl".into()
+    }
     fn fragment_shader() -> ShaderRef {
         "shaders/tilemap.wgsl".into()
     }
